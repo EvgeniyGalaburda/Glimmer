6f5113ac46a5b2b3c2e8f3a0f80eb455e2c520ac
Evgeniy Galaburda <evgeniygalaburda@gmail.com>
2024-09-23 20:05:08 +0300

    Auth

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -5,7 +5,7 @@ const connectMongoDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDb connected: ${conn.connection.host}`);
     } catch (error) {
-        console.log(`Error connecting to MongoDb: ${errer.message}`)
+        console.log(`Error connecting to MongoDb: ${error.message}`)
         process.exit(1);
     }
 }
