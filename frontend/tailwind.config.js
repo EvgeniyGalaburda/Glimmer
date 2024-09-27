af8d0b739307e1a2d87cdf635f291eb8aae7b190
Evgeniy Galaburda <evgeniygalaburda@gmail.com>
2024-09-28 00:54:13 +0300

    Auth UI

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,28 @@
+import daisyui from 'daisyui'
+import daisyuiUIThemes from 'daisyui/src/theming/themes'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui,
+    require('tailwindcss-animated')
+  ],
+
+  daisyui:{
+    themes: [
+      "cupcake",
+      {
+        black: {
+          ...daisyuiUIThemes['dracula'],
+        }
+      }
+    ]
+  }
+}
