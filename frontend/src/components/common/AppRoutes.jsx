fe7a4144cc9e79176c862e425c98d786e562ad10
Evgeniy Galaburda <evgeniygalaburda@gmail.com>
2024-10-04 23:39:31 +0300

    Profile UI

--- frontend/src/components/common/AppRoutes.jsx
+++ frontend/src/components/common/AppRoutes.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom"
 import HomePage from "../../pages/home/HomePage"
 import Sidebar from "./Sidebar"
 import NotificationPage from "../../pages/notifications/NotificationPage"
+import ProfilePage from "../../pages/profile/ProfilePage"
+import RightPanel from "./RightPanel"
 
 const AppRoutes = () => {
   return (
@@ -10,7 +12,9 @@ const AppRoutes = () => {
     <Routes>
         <Route index element={<HomePage/>}/>
         <Route path="/notifications" element={<NotificationPage/>}/>
+        <Route path='/profile/:username' element={<ProfilePage/>}/>
     </Routes>
+    <RightPanel/>
     </>
   )
 }
--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Posts from "../../components/common/Posts";
 import CreatePost from "./CreatePost";
-import RightPanel from "../../components/common/RightPanel";
+
 
 export default function HomePage() {
   const [feedType, setFeedType] = useState("forYou");
@@ -33,7 +33,6 @@ export default function HomePage() {
 
         <Posts/>
       </div>
-      <RightPanel/>
     </>
   );
 }
