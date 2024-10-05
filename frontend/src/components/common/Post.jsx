9cc4927bb1cde5dab86adbebbab1833340b19f7a
Evgeniy Galaburda <evgeniygalaburda@gmail.com>
2024-10-05 21:20:06 +0300

    Delete post added

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -5,19 +5,47 @@ import { FaRegBookmark } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+import LoadingSpinner from '../common/LoadingSpinner';
 
 const Post = ({ post }) => {
+	const queryClient = useQueryClient();
 	const [comment, setComment] = useState("");
+	const {data: authUser} = useQuery({queryKey: ['authUser']});
+	const {mutate: deletePost, isPending} = useMutation({
+		mutationFn: async () => {
+			try {
+				const res = await fetch(`/api/posts/delete/${post._id}`, {
+					method: 'DELETE',
+				})
+
+				const data = await res.json();
+
+				if(!res.ok) throw new Error(data.error)
+				else {
+					toast.success("Post deleted successfully!");
+					queryClient.invalidateQueries({queryKey: ['posts']});
+				}
+				return data;
+			} catch (error) {
+				throw new Error(error)
+			}
+		}
+	})
 	const postOwner = post.user;
 	const isLiked = false;
 
-	const isMyPost = true;
+	const isMyPost = authUser._id === post.user._id;
 
 	const formattedDate = "1h";
 
 	const isCommenting = false;
 
-	const handleDeletePost = () => {};
+	const handleDeletePost = () => {
+		deletePost();
+	};
 
 	const handlePostComment = (e) => {
 		e.preventDefault();
@@ -45,7 +73,8 @@ const Post = ({ post }) => {
 						</span>
 						{isMyPost && (
 							<span className='flex justify-end flex-1'>
-								<FaTrash className='cursor-pointer hover:text-red-500' onClick={handleDeletePost} />
+								{isPending && (<LoadingSpinner size="sm"/>)}
+								{!isPending && (<FaTrash className='cursor-pointer hover:text-red-500' onClick={handleDeletePost} />)}
 							</span>
 						)}
 					</div>
